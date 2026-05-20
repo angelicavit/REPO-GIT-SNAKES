b5082269e4a38cb15ee85a7e8e082a55b593039d
angelicavit <vitalescruzreneangelica@gmail.com>
2026-05-20 11:58:52 -0600

    Creación de componentes

---
+++ src/app/components/detalle-proyecto/detalle-proyecto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-proyecto',
+  imports: [],
+  templateUrl: './detalle-proyecto.html',
+  styleUrl: './detalle-proyecto.css',
+})
+export class DetalleProyecto {}
