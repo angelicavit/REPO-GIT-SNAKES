959adc6d1b77a23fe15e7aeb535e4a890f12bffd
angelicavit <vitalescruzreneangelica@gmail.com>
2026-05-24 16:41:57 -0600

    Eliminacion de componentes repetidos

--- src/app/components/detalle-proyecto/detalle-proyecto.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-detalle-proyecto',
-  imports: [],
-  templateUrl: './detalle-proyecto.html',
-  styleUrl: './detalle-proyecto.css',
-})
-export class DetalleProyecto {}
